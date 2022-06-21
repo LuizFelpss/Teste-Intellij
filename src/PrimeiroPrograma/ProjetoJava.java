8cf97f004db594190d32749559541af79a781308
Felipe <yfelipelv@gmail.com>
2022-06-20 23:06:11 -0300

    First Commit

---
+++ src/PrimeiroPrograma/ProjetoJava.java
@@ -0,0 +1,15 @@
+package PrimeiroPrograma;
+
+import PrimeiroPrograma.model.Gato;
+
+public class ProjetoJava {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 10;
+        System.out.println("O Resultado é: " + (a + b));*/
+    }
+}
